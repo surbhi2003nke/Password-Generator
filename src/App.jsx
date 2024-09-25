71fb1ffee375a2567af1ecba300aa9a0fdf226b4
Surbhi Gupta <surbhi2003nke@gmail.com>
2024-09-25 21:43:40 +0530

    passward svg

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,8 @@
 import React from "react";
 import { useState, useCallback, useEffect, useRef } from "react";
+import download from "./images/download.svg"
+import replay from "./images/replay.svg"
+
 
 function App() {
   const [password, setPassword] = useState("");
@@ -12,7 +15,7 @@ function App() {
   });
 
   const [leastSelected, setLeastSelected] = useState(1);
-  const passwordRef = useRef(null)
+  const passwordRef = useRef(null);
 
   const passwardGenerator = useCallback(() => {
     let pass = "";
@@ -27,12 +30,11 @@ function App() {
       pass += str.charAt(charIndex);
     }
     setPassword(pass);
-  },[dependency, leastSelected])
+  }, [dependency, leastSelected]);
 
   useEffect(() => {
     passwardGenerator();
   }, [dependency, passwardGenerator]);
-
 
   const handleRange = (e) => {
     setDependency((prevState) => ({
@@ -44,23 +46,25 @@ function App() {
   const copyPasswordToClipboard = useCallback(() => {
     passwordRef.current?.select();
     passwordRef.current?.setSelectionRange(0, 999);
-    window.navigator.clipboard.writeText(password)
+    window.navigator.clipboard.writeText(password);
 
-    navigator.clipboard.writeText(password).then(() => {
-      alert("Password copied to clipboard!");
-    }).catch((error) => {
-      console.error("Failed to copy the text to clipboard: ", error);
-    })
-    
-  }, [password])
-
-
+    navigator.clipboard
+      .writeText(password)
+      .then(() => {
+        alert("Password copied to clipboard!");
+      })
+      .catch((error) => {
+        console.error("Failed to copy the text to clipboard: ", error);
+      });
+  }, [password]);
 
   return (
     <div className="flex justify-center py-8">
       <div className="w-[85%]">
         <div className="flex flex-col justify-center items-center m-4">
-          <h1 className="text-5xl font-bold p-5 text-center">Random Password Generator</h1>
+          <h1 className="text-5xl font-bold p-5 text-center">
+            Random Password Generator
+          </h1>
           <h3 className="from-neutral-100 p-2 text-xl text-center">
             Create strong and secure passwords to keep your account safe online.
           </h3>
@@ -69,13 +73,16 @@ function App() {
         <div className="flex tracking-wide text-[22px] flex-col lg:flex-row">
           {/* img parameters */}
           <div className="flex basis-2/5">
-            <img src="./public/download.svg" alt="" className="hidden lg:block"/>
+            <img
+              src={download}
+              alt=""
+              className="hidden lg:block"
+            />
           </div>
 
           {/* input parameters  */}
           <div className="flex flex-col basis-3/5 gap-4 my-4">
-
-{/*----------------------------------------------------------------------------------------------------*/}          
+            {/*----------------------------------------------------------------------------------------------------*/}
             <div className="flex basis-1/3 border-red-200 border-[1px] gap-1 justify-center items-center pl-5 text-[20px]">
               <div className="flex flex-row basis-4/5 w-full relative">
                 <div className="outline-none w-[100%] py-2 px-3 rounded-3xl shadow-inner bg-blue-100 ">
@@ -88,29 +95,30 @@ function App() {
                   />
                 </div>
                 <div className="flex justify-center items-center absolute py-2 px-4 right-0 top-0 bottom-0 ">
-                  <button   
+                  <button
                     className="w-4 hover:scale-110 hover:ease-in-out duration-300 font-semibold flex justify-center items-center text-center"
                     onClick={() => passwardGenerator()}
                   >
-                    <img src="./public/replay.svg" alt="Replay Button" className="flex justify-center items-center"/>
+                    <img
+                      src={replay}
+                      alt="Replay Button"
+                      className="flex justify-center items-center"
+                    />
                   </button>
                 </div>
               </div>
               <div className="basis-1/5 flex justify-center items-center text-center">
-                <button 
+                <button
                   className="bg-[#0070F6] px-5 py-2 rounded-3xl text-center hover:scale-110 hover:ease-in-out duration-300 font-semibold text-white"
                   onClick={copyPasswordToClipboard}
-                  
                 >
                   Copy
                 </button>
               </div>
             </div>
-{/*  ---------------------------------------------------------------------------------------------------------*/}
+            {/*  ---------------------------------------------------------------------------------------------------------*/}
 
-
-
-{/*  ---------------------------------------------------------------------------------------------------------*/}
+            {/*  ---------------------------------------------------------------------------------------------------------*/}
             {/* length slider */}
             <div className="flex basis-1/3 border-blue-200 border-[1px] justify-center items-center pl-5">
               <div className="flex flex-col basis-2/5">
@@ -124,16 +132,17 @@ function App() {
                   {/* decrease count function */}
                   <button
                     className="w-12 h-12 rounded-full m-1 bg-transparent border-[1px] hover:bg-[#e3e3e3] hover:scale-110 hover:ease-in-out duration-300 hover:text-[#181A1B] font-bold text-white"
-                    
-                    onClick={(e) => setDependency(prevState => {
-                      if (prevState.rangeLen > 1) {
-                        return {
-                          ...prevState,
-                          rangeLen: prevState.rangeLen - 1
-                        };
-                      } else {
-                        return prevState;  // If the condition is not met, return the state unchanged
-                      }})
+                    onClick={(e) =>
+                      setDependency((prevState) => {
+                        if (prevState.rangeLen > 1) {
+                          return {
+                            ...prevState,
+                            rangeLen: prevState.rangeLen - 1,
+                          };
+                        } else {
+                          return prevState; // If the condition is not met, return the state unchanged
+                        }
+                      })
                     }
                   >
                     <i class="bx bx-minus"></i>
@@ -152,15 +161,17 @@ function App() {
                 <div className="flex justify-center items-center ">
                   <button
                     className="w-12 h-12 rounded-full m-1 bg-transparent border-[1px] hover:bg-[#e3e3e3] hover:scale-110 hover:ease-in-out duration-300 hover:text-[#181A1B] font-bold text-white "
-                    onClick={(e) => setDependency(prevState => {
-                      if (prevState.rangeLen < 50) {
-                        return {
-                          ...prevState,
-                          rangeLen: prevState.rangeLen + 1
-                        };
-                      } else {
-                        return prevState;  // If the condition is not met, return the state unchanged
-                      }})
+                    onClick={(e) =>
+                      setDependency((prevState) => {
+                        if (prevState.rangeLen < 50) {
+                          return {
+                            ...prevState,
+                            rangeLen: prevState.rangeLen + 1,
+                          };
+                        } else {
+                          return prevState; // If the condition is not met, return the state unchanged
+                        }
+                      })
                     }
                   >
                     <i class="bx bx-plus"></i>
@@ -169,19 +180,14 @@ function App() {
               </div>
             </div>
 
-
-{/*  ---------------------------------------------------------------------------------------------------------*/}
+            {/*  ---------------------------------------------------------------------------------------------------------*/}
             {/* check box input section */}
             <div className="flex basis-1/3 border-yellow-200 border-[1px] justify-center items-center pl-5">
               <div className="basis-2/5 ">Characters Used: </div>
 
-
-
-
-  {/*  ---------------------------------------------------------------------------------------------------------*/}
+              {/*  ---------------------------------------------------------------------------------------------------------*/}
               {/* checkbox input */}
               <div className="basis-4/5 flex font-semibold">
-
                 {/* upper case checkbox  */}
                 <div className="basis-1/4">
                   <input
@@ -190,7 +196,7 @@ function App() {
                     id="UppercaseInput"
                     className="w-5 h-5 rounded-full mx-2 bg-none"
                     onClick={(e) =>
-                      setDependency(prevState => ({
+                      setDependency((prevState) => ({
                         ...prevState,
                         uppercase: !prevState.uppercase,
                       }))
@@ -206,7 +212,7 @@ function App() {
                     id="LowercaseInput"
                     className="w-5 h-5 rounded-full mx-2 bg-none"
                     onClick={(e) =>
-                      setDependency(prevState => ({
+                      setDependency((prevState) => ({
                         ...prevState,
                         lowercase: !prevState.lowercase,
                       }))
@@ -222,7 +228,7 @@ function App() {
                     id="NumberInput"
                     className="w-5 h-5 rounded-full mx-2 bg-none"
                     onClick={(e) =>
-                      setDependency(prevState => ({
+                      setDependency((prevState) => ({
                         ...prevState,
                         numbers: !prevState.numbers,
                       }))
@@ -238,7 +244,7 @@ function App() {
                     id="SpecialCharacterInput"
                     className="w-5 h-5 bg-gray-100 border-none rounded-xl"
                     onClick={(e) =>
-                      setDependency(prevState => ({
+                      setDependency((prevState) => ({
                         ...prevState,
                         specialCharacter: !prevState.specialCharacter,
                       }))
@@ -246,10 +252,8 @@ function App() {
                   />
                   <label htmlFor="SpecialCharacterInput">@#$</label>
                 </div>
-                
               </div>
-{/*  ---------------------------------------------------------------------------------------------------------*/}
-
+              {/*  ---------------------------------------------------------------------------------------------------------*/}
             </div>
           </div>
         </div>
@@ -259,5 +263,3 @@ function App() {
 }
 
 export default App;
-
-
